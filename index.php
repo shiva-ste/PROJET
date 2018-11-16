1828bdcae685b990620c0615d5d72119cbffb196
Stephane GUIBERTEAU <43678749+shiva-ste@users.noreply.github.com>
2018-11-16 14:40:30 +0100

    Premiere version du projet

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PROJET </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+<body>
+
+<div class="container">
+
+    <div id="nav" class="row text-center">
+        <div class="col">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Projet </a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Plan  <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Do </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Check </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Acte </a>
+                    </li>
+                    </ul>
+                </div>
+            </nav>  <!-- / fin de la navigation  -->
+        </div> <!-- / fin de la colonne -->
+    </div> <!-- / fin de la ligne id=nav --> 
+
+    <div id="content" class="row text-center p-4">
+        <div class="col-6 offset-3">
+
+
+        </div>
+    </div> <!-- / fin de la ligne id=content --> 
+
+    <div id="footer" class="row text-center">
+        <div class="col-6 offset-3">
+
+        </div>
+    </div> <!-- / fin de la ligne id=footer --> 
+</div> <!-- / fin du container  --> 
+
+</body>
+</html>
